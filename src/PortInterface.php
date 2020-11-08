bf4eed2b37bc0af4a7caecc5e1628c9469bc0e44
Mohammad Maghsoudi <didi1987@gmail.com>
2020-11-08 12:24:09 +0330

    Add national code

--- src/PortInterface.php
+++ src/PortInterface.php
@@ -5,97 +5,103 @@ namespace Vandar\Gateway;
 interface PortInterface
 {
 
-  /**
-   * This method use for set price in Rial.
-   *
-   * @param int $amount in Rial
-   *
-   * @return $this
-   */
-  public function set($amount);
+    /**
+     * This method use for set price in Rial.
+     *
+     * @param int $amount in Rial
+     *
+     * @return $this
+     */
+    public function set($amount);
 
-  /**
-   * This method use for done everything that necessary before redirect to port.
-   *
-   * @return $this
-   */
-  public function ready();
+    /**
+     * This method use for done everything that necessary before redirect to port.
+     *
+     * @return $this
+     */
+    public function ready();
 
-  /**
-   * Get ref id, in some ports ref id has a different name such as authority
-   *
-   * @return int|string
-   */
-  public function refId();
+    /**
+     * Get ref id, in some ports ref id has a different name such as authority
+     *
+     * @return int|string
+     */
+    public function refId();
 
-  /**
-   * Return tracking code
-   *
-   * @return int|string
-   */
-  public function trackingCode();
+    /**
+     * Return tracking code
+     *
+     * @return int|string
+     */
+    public function trackingCode();
 
-  /**
-   * Get port id, $this->portId
-   *
-   * @return int
-   */
-  public function getPortName();
+    /**
+     * Get port id, $this->portId
+     *
+     * @return int
+     */
+    public function getPortName();
 
-  /**
-   * Get transaction id
-   *
-   * @return int|null
-   */
-  public function transactionId();
+    /**
+     * Get transaction id
+     *
+     * @return int|null
+     */
+    public function transactionId();
 
-  /**
-   * Return card number
-   *
-   * @return string
-   */
-  public function cardNumber();
+    /**
+     * Return card number
+     *
+     * @return string
+     */
+    public function cardNumber();
 
-  /**
-   * Sets callback url
-   *
-   * @return string
-   */
-  public function setCallback($url);
+    /**
+     * Sets callback url
+     *
+     * @return string
+     */
+    public function setCallback($url);
 
-  public function setCellNumber($cellNumner);
+    public function setCellNumber($cellNumner);
 
-  public function setMerchant($merchant);
+    public function setMerchant($merchant);
 
-  public function setMerchantPassword($password);
+    public function setMerchantPassword($password);
 
-  /**
-   * Gets callback url
-   *
-   * @return string
-   */
-  public function getCallback();
+//  public function setValidCardNumber($valid_card_number);
 
-  public function getMerchant();
+    public function setNationalCode($national_code);
 
-  public function getMerchantPassword();
+    public function getNationalCode($national_code);
 
-  /**
-   * This method use for redirect to port
-   *
-   * @return mixed
-   */
-  public function redirect();
+    /**
+     * Gets callback url
+     *
+     * @return string
+     */
+    public function getCallback();
 
-  /**
-   * Return result of payment
-   * If result is done, return true, otherwise throws an related exception
-   *
-   * @param object $transaction row of transaction in database
-   *
-   * @return $this
-   */
-  public function verify($transaction);
+    public function getMerchant();
 
-  public function refund($transaction);
+    public function getMerchantPassword();
+
+    /**
+     * This method use for redirect to port
+     *
+     * @return mixed
+     */
+    public function redirect();
+
+    /**
+     * Return result of payment
+     * If result is done, return true, otherwise throws an related exception
+     *
+     * @param object $transaction row of transaction in database
+     *
+     * @return $this
+     */
+    public function verify($transaction);
+
+    public function refund($transaction);
 }
--- src/Saman/Saman.php
+++ src/Saman/Saman.php
@@ -10,251 +10,263 @@ use Vandar\Gateway\PortInterface;
 
 class Saman extends PortAbstract implements PortInterface
 {
-  /**
-   * Address of main SOAP server
-   *
-   * @var string
-   */
-  protected $serverUrl = 'https://sep.shaparak.ir/verifyTxnRandomSessionkey/ipg/VerifyTranscation';
+    /**
+     * Address of main SOAP server
+     *
+     * @var string
+     */
+    protected $serverUrl = 'https://sep.shaparak.ir/verifyTxnRandomSessionkey/ipg/VerifyTranscation';
 
-  /**
-   * getTokenUrl
-   *
-   * @var string
-   */
-  protected $getTokenUrl = 'https://sep.shaparak.ir/OnlinePG/OnlinePG';
+    /**
+     * getTokenUrl
+     *
+     * @var string
+     */
+    protected $getTokenUrl = 'https://sep.shaparak.ir/OnlinePG/OnlinePG';
 
-  /**
-   * sendTokenUrl
-   *
-   * @var string
-   */
-  protected $sendTokenUrl = 'https://sep.shaparak.ir/OnlinePG/SendToken?token=';
+    /**
+     * sendTokenUrl
+     *
+     * @var string
+     */
+    protected $sendTokenUrl = 'https://sep.shaparak.ir/OnlinePG/SendToken?token=';
 
-  /**
-   * {@inheritdoc}
-   */
-  public function set($amount)
-  {
-    $this->amount = $amount;
+    /**
+     * {@inheritdoc}
+     */
+    public function set($amount)
+    {
+        $this->amount = $amount;
 
-    return $this;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function ready()
-  {
-    $this->newTransaction();
-
-    return $this;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function redirect()
-  {
-    $client = new Client();
-
-    $response = $client->request(
-      'post',
-      $this->getTokenUrl,
-      [
-        'headers' => [
-          'Content-Type' => 'application/json'
-        ],
-        'json' => [
-          'Action' => 'token',
-          'TerminalId' => $this->getMerchant(),
-          'Amount' => $this->amount,
-          'CellNumber' => $this->getCellNumber(),
-          'ResNum' => $this->transactionId(),
-          'RedirectUrl' => $this->getCallback()
-        ]
-      ]
-    );
-
-    $responseContent = json_decode($response->getBody()->getContents(), true);
-
-    $this->transactionSetToken($responseContent['token']);
-
-    $go = $this->sendTokenUrl . $responseContent['token'];
-    // dd($data);
-    return redirect()->away($go);
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function verify($transaction)
-  {
-    parent::verify($transaction);
-
-    if ($transaction->merchant_id != null && $transaction->merchant_password != null) {
-      $this->setMerchant($transaction->merchant_id);
-      $this->setMerchantPassword($transaction->merchant_password);
+        return $this;
     }
 
-    $this->userPayment();
-    $this->verifyPayment();
+    /**
+     * {@inheritdoc}
+     */
+    public function ready()
+    {
+        $this->newTransaction();
 
-    return $this;
-  }
-
-  /**
-   * Sets callback url
-   * @param $url
-   */
-  function setCallback($url)
-  {
-    $this->callbackUrl = $url;
-    return $this;
-  }
-
-  function setCellNumber($cellNumner)
-  {
-    $this->cellNumber = $cellNumner;
-    return $this;
-  }
-
-  function getCellNumber()
-  {
-    return $this->cellNumber;
-  }
-
-  function setMerchant($merchant)
-  {
-    $this->merchant = $merchant;
-    return $this;
-  }
-
-  function getMerchant()
-  {
-    if (!$this->merchant)
-      $this->merchant = $this->config->get('gateway.saman.merchant');
-
-    return $this->merchant;
-  }
-
-  function setMerchantPassword($password)
-  {
-    $this->merchant_password = $password;
-    return $this;
-  }
-
-  function getMerchantPassword()
-  {
-    if (!$this->merchant_password)
-      $this->merchant_password = $this->config->get('gateway.saman.password');
-
-    return $this->merchant_password;
-  }
-
-  /**
-   * Gets callback url
-   * @return string
-   */
-  function getCallback()
-  {
-    if (!$this->callbackUrl)
-      $this->callbackUrl = $this->config->get('gateway.saman.callback-url');
-
-    $url = $this->makeCallback($this->callbackUrl, ['transaction_id' => $this->transactionId()]);
-
-    return $url;
-  }
-
-
-  /**
-   * Check user payment
-   *
-   * @return bool
-   *
-   * @throws SamanException
-   */
-  protected function userPayment()
-  {
-    $this->refId = Input::get('RefNum');
-    $this->trackingCode = Input::get('TraceNo');
-    $this->cardNumber = Input::get('SecurePan');
-    $payRequestRes = Input::get('State');
-    $payRequestResCode = Input::get('StateCode');
-
-    if ($payRequestRes == 'OK') {
-      return true;
+        return $this;
     }
 
-    $this->transactionFailed();
-    $this->newLog($payRequestResCode, @SamanException::$errors[$payRequestRes]);
-    throw new SamanException($payRequestRes);
-  }
+    /**
+     * {@inheritdoc}
+     */
+    public function redirect()
+    {
+        $client = new Client();
 
+        $response = $client->request(
+            'post',
+            $this->getTokenUrl,
+            [
+                'headers' => [
+                    'Content-Type' => 'application/json'
+                ],
+                'json' => [
+                    'Action' => 'token',
+                    'TerminalId' => $this->getMerchant(),
+                    'Amount' => $this->amount,
+                    'CellNumber' => $this->getCellNumber(),
+                    'ResNum' => $this->transactionId(),
+                    'RedirectUrl' => $this->getCallback(),
+                    'NationalCode' => $this->getNationalCode(),
+                    'CheckIfNationalCodeMatchesPan' => ($this->getNationalCode() != null) ? true : false
+                ]
+            ]
+        );
 
-  /**
-   * Verify user payment from bank server
-   *
-   * @return bool
-   *
-   * @throws SamanException
-   * @throws SoapFault
-   */
-  protected function verifyPayment()
-  {
+        $responseContent = json_decode($response->getBody()->getContents(), true);
 
-    $fields = array(
-      "merchantID" => $this->getMerchant(),
-      "RefNum" => $this->refId,
-      "password" => $this->getMerchantPassword(),
-    );
+        $this->transactionSetToken($responseContent['token']);
 
-    try {
-      $client = new Client();
+        $go = $this->sendTokenUrl . $responseContent['token'];
+        // dd($data);
+        return redirect()->away($go);
+    }
 
-      $response = $client->request(
-        'post',
-        $this->serverUrl,
-        [
-          'headers' => [
-            'Content-Type' => 'application/json'
-          ],
-          'json' => [
-            'RefNum' => $this->refId,
-            'TerminalNumber' => $this->getMerchant(),
-            'NationalCode' => "",
-            'IgnoreNationalcode' => true
-          ]
-        ]
-      );
-      $responseContent = json_decode($response->getBody()->getContents(), true);
-    } catch (\Exception $e) {
-      $this->transactionFailed();
-      $this->newLog('restFault', $e->getMessage());
-      throw $e;
+    /**
+     * {@inheritdoc}
+     */
+    public function verify($transaction)
+    {
+        parent::verify($transaction);
+
+        if ($transaction->merchant_id != null && $transaction->merchant_password != null) {
+            $this->setMerchant($transaction->merchant_id);
+            $this->setMerchantPassword($transaction->merchant_password);
+        }
+
+        $this->userPayment();
+        $this->verifyPayment();
+
+        return $this;
+    }
+
+    /**
+     * Sets callback url
+     * @param $url
+     */
+    function setCallback($url)
+    {
+        $this->callbackUrl = $url;
+        return $this;
+    }
+
+    function setCellNumber($cellNumner)
+    {
+        $this->cellNumber = $cellNumner;
+        return $this;
+    }
+
+    function getCellNumber()
+    {
+        return $this->cellNumber;
+    }
+
+    function setMerchant($merchant)
+    {
+        $this->merchant = $merchant;
+        return $this;
+    }
+
+    function getMerchant()
+    {
+        if (!$this->merchant)
+            $this->merchant = $this->config->get('gateway.saman.merchant');
+
+        return $this->merchant;
+    }
+
+    function setMerchantPassword($password)
+    {
+        $this->merchant_password = $password;
+        return $this;
+    }
+
+    function getMerchantPassword()
+    {
+        if (!$this->merchant_password)
+            $this->merchant_password = $this->config->get('gateway.saman.password');
+
+        return $this->merchant_password;
+    }
+
+    //  function setValidCardNumber($valid_card_number)
+//  {
+//    $this->valid_card_number = $valid_card_number;
+//    return $this;
+//  }
+
+    function setNationalCode($national_code)
+    {
+        $this->national_code = $national_code;
+        return $this;
+    }
+
+    function getNationalCode()
+    {
+        return $this->national_code;
+    }
+
+    /**
+     * Gets callback url
+     * @return string
+     */
+    function getCallback()
+    {
+        if (!$this->callbackUrl)
+            $this->callbackUrl = $this->config->get('gateway.saman.callback-url');
+
+        $url = $this->makeCallback($this->callbackUrl, ['transaction_id' => $this->transactionId()]);
+
+        return $url;
     }
 
 
-    if ($responseContent['Success'] && $responseContent['TransactionDetail']['OrginalAmount'] == $this->amount) {
-      $this->transactionSucceed();
-      return true;
-    }
+    /**
+     * Check user payment
+     *
+     * @return bool
+     *
+     * @throws SamanException
+     */
+    protected function userPayment()
+    {
+        $this->refId = Input::get('RefNum');
+        $this->trackingCode = Input::get('TraceNo');
+        $this->cardNumber = Input::get('SecurePan');
+        $payRequestRes = Input::get('State');
+        $payRequestResCode = Input::get('StateCode');
 
-    //Reverse Transaction
-    if ($response > 0) {
-      try {
-        $soap = new SoapClient($this->serverUrl);
-        $response = $soap->ReverseTransaction($fields["RefNum"], $fields["merchantID"], $fields["password"], $response);
-      } catch (\SoapFault $e) {
+        if ($payRequestRes == 'OK') {
+            return true;
+        }
+
         $this->transactionFailed();
-        $this->newLog('SoapFault', $e->getMessage());
-        throw $e;
-      }
+        $this->newLog($payRequestResCode, @SamanException::$errors[$payRequestRes]);
+        throw new SamanException($payRequestRes);
     }
 
-    //
-    $this->transactionFailed();
-    $this->newLog($response, SamanException::$errors[$response]);
-    throw new SamanException($response);
-  }
+
+    /**
+     * Verify user payment from bank server
+     *
+     * @return bool
+     *
+     * @throws SamanException
+     * @throws SoapFault
+     */
+    protected function verifyPayment()
+    {
+        try {
+            $client = new Client();
+
+            $response = $client->request(
+                'post',
+                $this->serverUrl,
+                [
+                    'headers' => [
+                        'Content-Type' => 'application/json'
+                    ],
+                    'json' => [
+                        'RefNum' => $this->refId,
+                        'TerminalNumber' => $this->getMerchant(),
+                        'NationalCode' => "",
+                        'IgnoreNationalcode' => true
+                    ]
+                ]
+            );
+            $responseContent = json_decode($response->getBody()->getContents(), true);
+        } catch (\Exception $e) {
+            $this->transactionFailed();
+            $this->newLog('restFault', $e->getMessage());
+            throw $e;
+        }
+
+
+        if ($responseContent['Success'] && $responseContent['TransactionDetail']['OrginalAmount'] == $this->amount) {
+            $this->transactionSucceed();
+            return true;
+        }
+
+        //Reverse Transaction
+        if ($response > 0) {
+            try {
+                $soap = new SoapClient($this->serverUrl);
+                $response = $soap->ReverseTransaction($fields["RefNum"], $fields["merchantID"], $fields["password"], $response);
+            } catch (\SoapFault $e) {
+                $this->transactionFailed();
+                $this->newLog('SoapFault', $e->getMessage());
+                throw $e;
+            }
+        }
+
+        //
+        $this->transactionFailed();
+        $this->newLog($response, SamanException::$errors[$response]);
+        throw new SamanException($response);
+    }
 }
